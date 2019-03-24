6d2af851c233c2de0d15f0681a5310c0807741d9
daniltor77 <daniltor77@mail.ru>
2019-03-25 00:58:26 +0300

    простая 2

---
+++ easy 2.py
@@ -0,0 +1,9 @@
+inputList1=['яблоко','морковь','банан','капуста','киви','сало','арбуз']
+
+inputList2=['яблоко','банан','киви','арбуз']
+
+
+for i in inputList2:
+	inputList1.remove(i)
+print (inputList1)
+input()
